aba8e0553cf8aea9bab6571c9802095eb39756f9
AeGON <aegon@pop-os.localdomain>
2023-04-04 12:49:26 +0530

    first commit

---
+++ src/Components/data.js
@@ -0,0 +1,75 @@
+export default [
+    {
+        title: 'UC: Product Catalog',
+        url: 'src/assets/Final BMP Diagrams/Use Case Diagrams/Use Case Buyer 2nd.svg'
+    },
+    {
+        title: 'UC: Shop Module',
+        url: 'src/assets/Final BMP Diagrams/Use Case Diagrams/Use Case Shop.svg'
+    },
+    {
+        title: 'UC: Home Page Module',
+        url: 'src/assets/Final BMP Diagrams/Use Case Diagrams/Use Case Home Page Module.jpg'
+    },
+    {
+        title: 'UC: System Manager',
+        url: 'src/assets/Final BMP Diagrams/Use Case Diagrams/Use Case System Manager 2nd.svg'
+    },
+    {
+        title: 'UC: Shopping Cart',
+        url: 'src/assets/Final BMP Diagrams/Use Case Diagrams/Use Case Cart.svg'
+    },
+    {
+        title: 'UC: Delevery Service Module',
+        url: 'src/assets/Final BMP Diagrams/Use Case Diagrams/Use Case Delivery Service Module.svg'
+    },
+    {
+        title: 'AD: User Authentication',
+        url: 'src/assets/Final BMP Diagrams/Activity Diagrams/Activity Diagram Authentication.svg'
+    },
+    {
+        title: 'AD: Place Order',
+        url: 'src/assets/Final BMP Diagrams/Activity Diagrams/Activity Diagram Place Order.svg'
+    },
+    {
+        title: 'Add Product in Shop',
+        url: 'src/assets/Final BMP Diagrams/Activity Diagrams/Activity Diagram Add Product.svg'
+    },
+    {
+        title: 'AD: Billing Process',
+        url: 'src/assets/Final BMP Diagrams/Activity Diagrams/Activity Diagram Billing.svg'
+    },
+    {
+        title: 'AD: Review Product',
+        url: 'src/assets/Final BMP Diagrams/Activity Diagrams/Activity Diagram Review.svg'
+    },
+    {
+        title: 'AD: Contact Customer Service',
+        url: 'src/assets/Final BMP Diagrams/Activity Diagrams/Activity Diagram Contact Customer Service.svg'
+    },
+    {
+        title: 'SD: Place Order',
+        url: 'src/assets/Final BMP Diagrams/Sequence Diagrams/Sequence Diagram For Placing Order.svg'
+    },
+    {
+        title: 'SD: Delivery Service',
+        url: 'src/assets/Final BMP Diagrams/Sequence Diagrams/Sequence Diagram For Delivery Service.svg'
+    },
+    {
+        title: 'SD: Return Product',
+        url: 'src/assets/Final BMP Diagrams/Sequence Diagrams/Sequence Diagram For Returning Product.svg'
+    },
+    {
+        title: 'CD: System/Application',
+        url: 'src/assets/Final BMP Diagrams/Class Diagrams/System Class Diagram.svg'
+    },
+    {
+        title: 'State: Product',
+        url: 'src/assets/Final BMP Diagrams/State diagram/Product State Chart Diagram.jpg'
+    },
+    {
+        title: 'State: Order',
+        url: 'src/assets/Final BMP Diagrams/State diagram/Order State Diagram.svg'
+    },
+
+]
